7e751f280a1fc5f607efa5eac4771423fc1e3692
hanlyjiang <hanlyjiang@outlook.com>
2021-01-13 10:24:01 +0800

    feature: 添加rest服务接口

--- src/main/kotlin/cn/hanlyjiang/springboot/springbootdemo1/SpringbootDemo1Application.kt
+++ src/main/kotlin/cn/hanlyjiang/springboot/springbootdemo1/SpringbootDemo1Application.kt
@@ -2,9 +2,12 @@ package cn.hanlyjiang.springboot.springbootdemo1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.PropertySource
 
 @SpringBootApplication
-class SpringbootDemo1Application
+class SpringbootDemo1Application {
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringbootDemo1Application>(*args)
--- src/main/kotlin/cn/hanlyjiang/springboot/springbootdemo1/controller/HelloController.kt
+++ src/main/kotlin/cn/hanlyjiang/springboot/springbootdemo1/controller/HelloController.kt
@@ -2,6 +2,7 @@ package cn.hanlyjiang.springboot.springbootdemo1.controller
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,6 +11,14 @@ class HelloController {
 
     @RequestMapping("/home")
     fun home(): String {
-        return "Hello SpringBoot!!!"
+        return "Hello SpringBoot !!!"
     }
+
+    // rest-service: https://spring.io/guides/gs/rest-service/
+    @RequestMapping("/name")
+    fun home(@RequestParam(value = "name", defaultValue = "没有名字") name: String):Greeting {
+        return Greeting(Math.random().toInt(), name)
+    }
+
+    data class Greeting(val id: Int, val value: String)
 }
